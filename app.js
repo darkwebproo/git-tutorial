48842571807301289189b0a1e2d134fbfeeb6796
shakil <shakil.nodejs@gmail.com>
2022-07-27 20:01:36 +0600

    this is object data type

--- app.js
+++ app.js
@@ -16,15 +16,27 @@ finance()
 
 Premative
 Number
+// var num = 23 2 32 0.4 5.3 53.3;
 
 String
+// var str = "I Love JavaScript"
 
 Boolean
-
-
+// var x = ture;
+// var y = false;
 
 Object
+// var name = {
+//     SHAKIL,
+//     IMRAN,
+//     BAPPY,
+//     MEHEDI,
+//     KAMRUL,
+//     MOSTAKIM
+// }
+
 Array
+// var arr = [name]
 
 Object
 
