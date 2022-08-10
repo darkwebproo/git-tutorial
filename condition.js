29e9bbc0bb51dde51b13ac72ac4f2dbd2ff61df3
shakil <shakil.nodejs@gmail.com>
2022-08-11 01:05:26 +0600

    this added conditinal js files

---
+++ condition.js
@@ -0,0 +1,19 @@
+// *************** CONDITIONALS JS ******************************** // 
+
+var biscuitPrice = 12;
+
+if (biscuitPrice === 10) {
+    console.log('I Will eat the biscuits');
+} else {
+    console.log('\n Khali cha is better \n');
+}
+
+// *********** DABLE CONDITIONALS JS ************ //     
+var jobPaiso = true;
+var savingAmount = 10000;
+
+if (jobPaiso == true && savingAmount > 20000) {
+    console.log('Aso tomar patri khuji');
+} else {
+    console.log('Tor kopale biya nai');
+}
