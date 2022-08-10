fa336c68d2a1e8ff5a94723dbaeb0422e2096ac7
shakil <shakil.nodejs@gmail.com>
2022-08-10 23:14:29 +0600

    this added app.js codified

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
-// ************** VARIABLE **************** //     
-var name = 'Shakil Ahmad';
+//  I LOVE JAVASCRIPT //////////////////////////////////////////////////////////////////
+
+// datatype //////////////////////////////////////////////////////////////////
+
+// Premative
+Number
+// var num = 23 2 32 0.4 5.3 53.3;
+var bananaPrice = 16;
+console.log(typeof(bananaPrice));
+
+String
+// var str = "I Love JavaScript"
+
+Boolean
+// var x = ture;
+// var y = false;
+
+Object
+// var name = {
+//     SHAKIL,
+//     IMRAN,
+//     BAPPY,
+//     MEHEDI,
+//     KAMRUL,
+//     MOSTAKIM
+// }
+
+Array
+// var arr = [name]
+
+Object
+
+Function
