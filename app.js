f0539668415e4311da57d951df7548438f0e5824
shakil <shakil.nodejs@gmail.com>
2022-10-12 22:20:48 +0600

    this is app.js branchB

--- app.js
+++ app.js
@@ -32,4 +32,29 @@ Object
 
 Function
 
-// skfhk jhdjkfjchuee kdm jiff abcdefghijklmnpq s
+
+
+var speech = "I am a   front-end developer and back end developer";
+var count = 0;
+for (i = 0; i < speech.length; i++) {
+    var chea = speech[i];
+    if (chea == " " && speech[i - 1] != " ") {
+        count++;
+    }
+}
+count++;
+console.log(count);
+// skfhk jhdjkfjchuee kdm jiff abcdefghijklmnpqs
+
+
+var biscuitPrice = 12;
+
+if (biscuitPrice === 10) {
+    console.log('I Will eat the biscuits');
+} else {
+    console.log('\n Khali cha is better \n');
+}
+
+// *********** DABLE CONDITIONALS JS ************ //
+
+console.log("this is practics my branchB");
