e6b3b852dd68a9278852c4f9d179647e11b43d41
shakil <shakil.nodejs@gmail.com>
2022-10-12 22:01:23 +0600

    change the wordcount method

--- app.js
+++ app.js
@@ -32,4 +32,16 @@ Object
 
 Function
 
-// skfhk jhdjkfjchuee kdm jiff abcdefghijklmnpq s
+
+
+var speech = "I am a   front-end developer and back end developer";
+var count = 0;
+for (i = 0; i < speech.length; i++) {
+    var chea = speech[i];
+    if (chea == " " && speech[i - 1] != " ") {
+        count++;
+    }
+}
+count++;
+console.log(count);
+// skfhk jhdjkfjchuee kdm jiff abcdefghijklmnpqs
