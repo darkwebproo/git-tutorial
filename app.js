61f148001f968ab91de9b1feb1fb4dfd84d9b04d
shakil <shakil.nodejs@gmail.com>
2022-10-12 22:30:44 +0600

    change the branchB file

--- app.js
+++ app.js
@@ -34,19 +34,6 @@ Function
 
 
 
-var speech = "I am a   front-end developer and back end developer";
-var count = 0;
-for (i = 0; i < speech.length; i++) {
-    var chea = speech[i];
-    if (chea == " " && speech[i - 1] != " ") {
-        count++;
-    }
-}
-count++;
-console.log(count);
-// skfhk jhdjkfjchuee kdm jiff abcdefghijklmnpqs
-
-
 var biscuitPrice = 12;
 
 if (biscuitPrice === 10) {
